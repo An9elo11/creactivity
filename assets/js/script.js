9ec2e93114994039c211473846be30194f8f3296
kouakan.assemian_ange_uriel <kouakan.assemian_ange_uriel@courrier.uqam.ca>
2025-05-08 13:21:02 -0400

    Première version

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     'images/logo/Creactivity_2.png',
     'images/logo/Creactivity_3.png',
     'images/logo/Creactivity_4.png',
-    'images/logo/Creactivity_5.png',]
+    'images/logo/Creactivity_5.png']
 
     let random = images[Math.floor(Math.random() * images.length)];
     document.getElementById('logo').src = random;
