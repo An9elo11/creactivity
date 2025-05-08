19aca0348b5879a78b23ca8d18d2bc75ac49b20e
kouakan.assemian_ange_uriel <kouakan.assemian_ange_uriel@courrier.uqam.ca>
2025-05-08 02:53:45 -0400

    Commit 1

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let images = ['images/logo/Creactivity_1.png',
+    'images/logo/Creactivity_2.png',
+    'images/logo/Creactivity_3.png',
+    'images/logo/Creactivity_4.png',
+    'images/logo/Creactivity_5.png',]
+
+    let random = images[Math.floor(Math.random() * images.length)];
+    document.getElementById('logo').src = random;
+})
