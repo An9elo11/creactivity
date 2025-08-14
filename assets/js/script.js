09801345c350f1506fe6fc9de813d8dc6f142a8f
kouakan.assemian_ange_uriel <kouakan.assemian_ange_uriel@courrier.uqam.ca>
2025-08-14 19:12:46 -0400

    Ajoute messagerie

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     'images/logo/Creactivity_2.png',
     'images/logo/Creactivity_3.png',
     'images/logo/Creactivity_4.png',
-    'images/logo/Creactivity_5.png']
+    'images/logo/Creactivity_5.png',
+    'images/logo/Creactivity_6.png',
+    'images/logo/Creactivity_7.png',
+    'images/logo/Creactivity_13.png',
+    'images/logo/Creactivity_14.png']
 
     let random = images[Math.floor(Math.random() * images.length)];
     document.getElementById('logo').src = random;
